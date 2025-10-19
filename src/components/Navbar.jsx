45ef379b7754f0756b006dc2d39abbde3770f15e
Jerome <jcrozz143@gmail.com>
2025-10-19 01:21:59 +0100

    Updated navabr

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar({ onToggle, isModalOn }) {
 
   return (
     <nav>
-      <h1>My Tasks</h1>
+      <h1>My Tasksghdh</h1>
       <button onClick={onToggle}>
         <span>{isModalOn ? <i class="ri-close-fill"></i> : <i class="ri-add-fill"></i>}</span>
       </button>
